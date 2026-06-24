ed88ac468c3c745b6380f9e20ca30c7dc1fb0429
XuNuoxi <qaqxunuoxi@outlook.com>
2026-06-24 21:04:02 +0800

    fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "xunuoxi.github.io/nuoxi-obsidian-garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
